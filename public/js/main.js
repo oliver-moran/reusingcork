93e548c19094007b9899345e3d6ce43c3ba2185d
Oliver Moran <oliver.moran@gmail.com>
2016-12-28 22:01:51 +0000

    reset location of more then 10km from Cork

--- public/js/main.js
+++ public/js/main.js
@@ -61,7 +61,14 @@ function startMap() {
     map.locate({setView: true});
     map.on('locationfound', function onLocationFound(e) {
         var radius = e.accuracy / 2; // meters
-        // map.setZoom(17);
+        var km = getDistanceFromLatLonInKm(CORK_LAT_LNG[0],CORK_LAT_LNG[1],e.latlng.lat,e.latlng.lng)
+        map.on("moveend", onMoveEnd);
+        function onMoveEnd(){
+            map.off("moveend", onMoveEnd);
+            if (km > 10) {
+                map.flyTo(CORK_LAT_LNG, 15);
+            }
+        }
     });
     if ($("html").hasClass("touch")) {
         map.dragging.disable();
@@ -313,4 +320,23 @@ function getParameterByName(name, url) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+// http://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
 }
